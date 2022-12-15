230816ada17714da4ac92dead06e57909df3d692
Aleksey Yesin <esinru@gmail.com>
2022-12-15 03:53:08 +0200

    Fix import

--- src/components/Home/Home.test.jsx
+++ src/components/Home/Home.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import * as redux from "react-redux";
 import * as router from "react-router-dom";
 import weatherThunks from "redux/weather/weatherThunks";
